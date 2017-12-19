cef8df8f6ea25324785b8fe331200f46dc93074b
KarolDmowski <dmowski.ka@gmail.com>
2017-12-19 19:33:49 +0100

    4.6 Calculator

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public static int addCalc(int firstNumber, int secondNumber){
+        int result = 0;
+        result = firstNumber + secondNumber;
+        return result;
+    }
+
+    public static int substractCalc(int firstNumber, int secondNumber){
+        int result = 0;
+        result = firstNumber - secondNumber;
+        return result;
+    }
+}
